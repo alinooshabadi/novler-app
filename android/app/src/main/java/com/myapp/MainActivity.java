5f881b0c1194469920e437809ff13b6140c572c2
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2020-05-22 13:57:17 +0430

    remove allow rtl

--- android/app/src/main/java/com/myapp/MainActivity.java
+++ android/app/src/main/java/com/myapp/MainActivity.java
@@ -6,9 +6,6 @@ import com.facebook.react.ReactActivity;
  import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
-
-  I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
-  sharedI18nUtilInstance.allowRTL(context, true);
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
